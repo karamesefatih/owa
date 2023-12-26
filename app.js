a684e498c1abf668f0fb0c3312cda4fbc6499784
fatih karameşe <f181220093@outlook.com>
2023-12-26 16:28:28 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,81 +1,81 @@
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// // CORS ayarları
-// app.use(cors());
-// app.get('/classify', (reqClassify, resClassify) => {
+// CORS ayarları
+app.use(cors());
+app.get('/classify', (reqClassify, resClassify) => {
 
-//   ///////
-//   const http = require("https");
-//   const options = {
-//     "method": "GET",
-//     "hostname": "lab1.veriket.com",
-//     "port": null,
-//     "path": "/Services.asmx/GetScopes?company=2d5d7ee8-57e0-467f-b479-d0ebfd5b5aa0",
-//     "headers": {
-//       "Accept": "*/*",
-//       //"User-Agent": "Thunder Client (https://www.thunderclient.com)"
-//     }
-//   };
+  ///////
+  const http = require("https");
+  const options = {
+    "method": "GET",
+    "hostname": "lab1.veriket.com",
+    "port": null,
+    "path": "/Services.asmx/GetScopes?company=2d5d7ee8-57e0-467f-b479-d0ebfd5b5aa0",
+    "headers": {
+      "Accept": "*/*",
+      //"User-Agent": "Thunder Client (https://www.thunderclient.com)"
+    }
+  };
   
-//   const req = http.request(options, function (res) {
-//     const chunks = [];
+  const req = http.request(options, function (res) {
+    const chunks = [];
   
-//     res.on("data", function (chunk) {
-//       chunks.push(chunk);
-//     });
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
   
-//     res.on("end", function () {
-//       const body = Buffer.concat(chunks);
-//       console.log("classify başarılı");
-//       resClassify.send(body.toString());
-//     });
-//   });
+    res.on("end", function () {
+      const body = Buffer.concat(chunks);
+      console.log("classify başarılı");
+      resClassify.send(body.toString());
+    });
+  });
   
-//   req.end();
-//   ////////
-//   });
+  req.end();
+  ////////
+  });
 
 
 
-// app.get('/sensitivities', (reqSensitivity, resSensitivity) => {
+ app.get('/sensitivities', (reqSensitivity, resSensitivity) => {
 
-//     ///////
-//     const http = require("https");
-//     const options = {
-//       "method": "GET",
-//       "hostname": "lab1.veriket.com",
-//       "port": null,
-//       "path": "/Services.asmx/GetSensitivities?company=2d5d7ee8-57e0-467f-b479-d0ebfd5b5aa0",
-//       "headers": {
-//         "Accept": "*/*",
-//         //"User-Agent": "Thunder Client (https://www.thunderclient.com)"
-//       }
-//     };
+    ///////
+    const http = require("https");
+    const options = {
+      "method": "GET",
+      "hostname": "lab1.veriket.com",
+      "port": null,
+      "path": "/Services.asmx/GetSensitivities?company=2d5d7ee8-57e0-467f-b479-d0ebfd5b5aa0",
+      "headers": {
+        "Accept": "*/*",
+        //"User-Agent": "Thunder Client (https://www.thunderclient.com)"
+      }
+    };
     
-//     const req = http.request(options, function (res) {
-//       const chunks = [];
+    const req = http.request(options, function (res) {
+      const chunks = [];
     
-//       res.on("data", function (chunk) {
-//         chunks.push(chunk);
-//       });
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
     
-//       res.on("end", function () {
-//         const body = Buffer.concat(chunks);
-//         console.log("senstivities başarılı");
-//         resSensitivity.send(body.toString());
-//       });
-//     });
+      res.on("end", function () {
+        const body = Buffer.concat(chunks);
+        console.log("senstivities başarılı");
+        resSensitivity.send(body.toString());
+      });
+    });
     
-//     req.end();
-//     ////////
-//     });
+    req.end();
+    ////////
+    });
 
 
 // app.get('/classifyResult', (reqClassifyResult, resClassifyResult) => {
 
-//       ///////
+//       /////
 //       const http = require("http");
 //       const options = {
 //         "method": "GET",
@@ -105,25 +105,25 @@
 //       req.end();
 //     });
 
-//     //http://demo.veriket.com/Services.asmx/ClassifyData?_clientKey=41H7wO535fLV3hHwN6hqbg==&company=2d5d7ee8-57e0-467f-b479-d0ebfd5b5aa0&data=Test&mac=1&ip=2&ip2=3&user=fati
-//     const port = process.env.PORT || 3000;
+    //http://demo.veriket.com/Services.asmx/ClassifyData?_clientKey=41H7wO535fLV3hHwN6hqbg==&company=2d5d7ee8-57e0-467f-b479-d0ebfd5b5aa0&data=Test&mac=1&ip=2&ip2=3&user=fati
+    const port = process.env.PORT || 3000;
 
-// app.get('/', (req1, res1) => {
-//     res1.send('Hello, World!');
-// });
-    
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', (req1, res1) => {
+    res1.send('Hello, World!');
 });
-
+    
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
